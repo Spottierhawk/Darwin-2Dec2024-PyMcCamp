f65e8ad0fa00c0fba9941d0a34ad09652449a2d0
Spottierhawk <spottierhawk@gmail.com>
2024-12-02 04:29:21 +0000

    Day 1
    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,56 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+#print("My name is Darwin")
+#print("I am 11 years old")
+#print("If I had $2,000, I would buy robux")
+
+#animal2="Penguin"
+#animal3="Lion"
+#print(animal1)
+#print(animal2)
+#print(animal3)
+
+#variable1=3
+#variable2=8
+#print(variable1*variable2)
+
+#name="Dog"
+#food="chicken"
+#print("I gave my "+ name + "a" + food)
+
+#def selfIntroduction(name, age):
+    
+    #print("My name is" +  name)
+    #print("I am evil")
+    #print("I am "+age +"this year")
+
+#selfIntroduction("Darwin","11")
+#selfIntroduction("john","13")
+
+def Calculator(num1,num2):
+    ans=num1+num2
+    print(ans)
+    ans=num1*num2
+    print(ans)
+    ans=num1/num2
+    print(ans)
+    ans=num1-num2
+    print(ans)
+
+
+
+Calculator(78787,14)
+
+
+
+
+
+
+
+
+
+
+
+
+
