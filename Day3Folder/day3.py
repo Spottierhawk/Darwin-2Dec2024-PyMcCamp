c043529e0d98f888393a933d9fc3e832eb75c85f
Spottierhawk <spottierhawk@gmail.com>
2024-12-04 04:09:47 +0000

    Day 3
    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,4 +1,74 @@
 # Write all your codes for Day 3 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day3")
+#username=input(" what is your name? ")
+#title=input(" what is your title? ")
+#command=input(" what is your command? ")
+
+#print(title  +  username + "," + " wants you to " + command)
+
+#num1 = int(input("what is your first number: "))
+#num2 = int(input("what is your second number: "))
+
+#first=str(num1 )+ " + " + str(num2 )
+#second=num1+num2
+
+#print(first + " = " + str(second))
+
+#first=str(num1 )+ " - " + str(num2 )
+#second=num1-num2
+
+#print(first + " = " + str(second))
+
+#first=str(num1 )+ " * " + str(num2 )
+#second=num1*num2
+
+#print(first + " = " + str(second))
+
+#first=str(num1 )+ " / " + str(num2 )
+#second=num1/num2
+
+#print(first + " = " + str(second))
+
+#numtomultiply=int(input("enter number to multiply: "))
+#numofmultiplication=int(input("how many times do you want to multiply: "))
+
+#for i in range(1,numofmultiplication + 1):
+    #print(str(i) + " x " + str(numtomultiply) + " = " + str(i*numtomultiply))
+
+#import random
+
+#randnum = random.randint(1,2)
+#userans = int(input("guess a number: "))
+
+#if userans == randnum:
+    #print("Congradulations ! You win nothin!!!!!!>:)")
+#elif userans < randnum:
+    #print("womp womp, safe and yet sorry, NERDO!!!!!!!! ")
+#else:
+    #print("Too daring, tsk tsk, yap yap, i did not run out of ideas or anything... Wait this is an idea! womp womp")
+
+#lessfive=5
+#userage=int(input("say your age"))
+#lessten=10
+#moreten=10
+
+
+
+#if userage < lessfive:
+    #print("You are below 5")
+#elif userage < lessten:
+    #print("you are between 5 to 9 years old")
+#else:
+    #print("you are at least 10 years old")
+
+
+
+
+
+
+
+
+
+
+
